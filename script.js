1de5f6c135b3a54f71d64adce91a80567581c10b
Raim <example@gmail.com>
2021-04-17 17:15:52 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+"use strict";
+
+//alert("Hello");
+
+//const result = confirm("Are u here?");
+//console.log(result);
+
+//const answer = prompt("Вам есть 18?");
+//console.log(typeof(answer));
+
+// здесь операция + используется для преобразования строку к числу
+//const ans = +prompt("Вам есть 18?");
+//console.log(typeof(ans));
+//console.log(ans + 12);
+
+//const category = "toys";
+//console.log("https://someurl.com/" + category + "/" + "5");
+
+//console.log(`https://someurl.com/${category}/5`);
+
+let incr = 10,
+    incr1 = 10,
+    decr = 10,
+    decr1 = 10;
+console.log(incr++, ++incr1, decr--, --decr1);
+
+console.log(2*4 === +"8"); // ==== для сторого сравнения по значению и по типу
