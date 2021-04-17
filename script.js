d62722f93b091e9a9ec1a5e5578180edfcf59916
Raim <example@gmail.com>
2021-04-17 18:49:05 +0600

    js training1

--- script.js
+++ script.js
@@ -18,10 +18,31 @@
 
 //console.log(`https://someurl.com/${category}/5`);
 
-let incr = 10,
+/*let incr = 10,
     incr1 = 10,
     decr = 10,
     decr1 = 10;
 console.log(incr++, ++incr1, decr--, --decr1);
 
 console.log(2*4 === +"8"); // ==== для сторого сравнения по значению и по типу
+*/
+
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+
+const personalMovieDB =  {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt("Один из последних просмотренных фильмов?", ""),
+      b = prompt("На сколько оцените его?", ""),
+      c = prompt("Один из последних просмотренных фильмов?", ""),
+      d = prompt("На сколько оцените его?", "");
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB.movies);
